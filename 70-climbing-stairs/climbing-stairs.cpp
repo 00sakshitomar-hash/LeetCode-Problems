8a57c102bc111c1174f81eac599ea49e245a11d8
Sakshi <0.0sakshitomar@gmail.com>
2026-08-06 12:37:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (38.90%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int solve(int n, vector<int>& dp){
+        if (n <= 1) return 1;
+
+	    if (dp[n] != -1) return dp[n];
+
+	    return dp[n] = solve(n-1,dp) + solve(n-2,dp);
+    }
+    
+    int climbStairs(int n) {
+
+        // int dp[1000000];
+        // memset(dp,-1,sizeof(dp));
+
+        vector<int> dp(n+1, -1);
+
+        return solve(n, dp);
+    }
+};
